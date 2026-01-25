4355cfd402544f6e22520bdc6a720c91f8e49bf5
jdoubled05 <jdoubled05@gmail.com>
2026-01-25 12:34:06 -0500

    feat: add CourtsMap component

---
+++ src/components/Map/CourtsMap.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View } from "react-native";
+import MapLibreGL from "@maplibre/maplibre-react-native";
+import { Court } from "@/src/services/courts";
+
+type CourtsMapProps = {
+  courts: Court[];
+};
+
+const DC_CENTER: [number, number] = [-77.0369, 38.9072];
+
+export function CourtsMap({ courts }: CourtsMapProps) {
+  return (
+    <MapLibreGL.MapView
+      style={{ flex: 1 }}
+      styleURL={MapLibreGL.StyleURL.Street}
+      logoEnabled={false}
+      compassEnabled
+    >
+      <MapLibreGL.Camera zoomLevel={11.5} centerCoordinate={DC_CENTER} />
+      {courts.map((court) => (
+        <MapLibreGL.PointAnnotation
+          key={court.id}
+          id={court.id}
+          coordinate={[court.longitude, court.latitude]}
+        >
+          <View className="h-3 w-3 rounded-full border border-white/80 bg-[#960000]" />
+        </MapLibreGL.PointAnnotation>
+      ))}
+    </MapLibreGL.MapView>
+  );
+}
